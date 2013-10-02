7b9e53ae7084b08e04823df48a198239993bb4cf
jpgarbanzo <jpgarbanzo@outlook.com>
2013-10-01 22:07:23 -0600

    Added CDS to _genedetails view, and also a couple of validations in the object build from the xmlelement, to check if an element exists before iterating with a foreach loop.
    Aldo cleaned some useless code from the view _genedetails

--- protected/modules/BLASTSearch/models/EBIGeneDetails.php
+++ protected/modules/BLASTSearch/models/EBIGeneDetails.php
@@ -11,15 +11,13 @@ class EBIGeneDetails extends CModel {
     public $Keyword;
     public $Title;
     public $Author;
-            
     public $Organism;
     public $TaxonLineage;
-    
     //these two properties are used to store an EBI link representatation
     //of $Organism and $TaxonLineage
     public $OrganismLink;
     public $TaxonLineageLinks;
-    
+    public $CDS;
     public $Sequence;
 
     // <editor-fold defaultstate="collapsed" desc="Methods">
@@ -49,11 +47,11 @@ class EBIGeneDetails extends CModel {
         // NOTE: you should only define rules for those attributes that
         // will receive user inputs.
         return array(
-            //array('Email,SequenceType,Sequence,Program,Database', 'required'),
-            //array('Email', 'length', 'max' => 500),
-            // The following rule is used by search().
-            // Please remove those attributes that should not be searched.
-            //array('Email,SequenceType,Sequence,Program,Database', 'safe', 'on' => 'search'),
+                //array('Email,SequenceType,Sequence,Program,Database', 'required'),
+                //array('Email', 'length', 'max' => 500),
+                // The following rule is used by search().
+                // Please remove those attributes that should not be searched.
+                //array('Email,SequenceType,Sequence,Program,Database', 'safe', 'on' => 'search'),
         );
     }
 
@@ -68,7 +66,8 @@ class EBIGeneDetails extends CModel {
             'Organism' => 'Organism',
             'TaxonLineage' => 'Lineage',
             'OrganismLink' => 'Organism',
-            'TaxonLineageLinks'=> 'TaxonLineage',
+            'TaxonLineageLinks' => 'TaxonLineage',
+            'CDS' => 'CDS location',
             'Sequence' => 'Sequence',
         );
     }
@@ -81,7 +80,8 @@ class EBIGeneDetails extends CModel {
             'Organism' => 'Organism',
             'TaxonLineage' => 'Lineage',
             'OrganismLink' => 'OrganismL',
-            'TaxonLineageLinks'=> 'LineageL',
+            'TaxonLineageLinks' => 'LineageL',
+            'CDS' => 'CDS',
             'Sequence' => 'Sequence',
         );
     }
@@ -96,72 +96,87 @@ class EBIGeneDetails extends CModel {
 
         $curl_response = curl_exec($curl);
         curl_close($curl);
-        
+
         $xml = new SimpleXMLElement($curl_response);
         return $xml;
     }
-    
-    
-    public function getEBIGeneDetailsFromSimpleXMLElement($pSimpleXMLElement){
+
+    public function getEBIGeneDetailsFromSimpleXMLElement($pSimpleXMLElement) {
         $gene_details = new EBIGeneDetails();
-        
+
         //entry is the node that contains all the relevant info
         $xml_details = $pSimpleXMLElement->children()->entry;
-        
+
         $gene_details->Keyword = $xml_details->keyword;
         //reference details
         $gene_details->Title = $xml_details->reference->title;
         //obtains the authors
-        foreach($xml_details->reference->author as $author)
-             $authors[] = $author;
-        $gene_details->Author = implode(', ', $authors);
+        if ($xml_details->reference->author) {
+            foreach ($xml_details->reference->author as $author)
+                $authors[] = $author;
+            $gene_details->Author = implode(', ', $authors);
+        }
+
         //Feature details
-        $feature_taxon = $xml_details->feature->taxon;
-        $gene_details->Organism = $feature_taxon->attributes()->{'scientificName'};
-        //obtains the lineages
-        foreach($feature_taxon->lineage->taxon as $lineage)
-            $taxon_lineage[] = $lineage->attributes()->{'scientificName'};   
-        $gene_details->TaxonLineage = $taxon_lineage; //implode(', ', $taxon_lineage);
+        //this loop might look a little dark, it's because both CDS and lineage
+        //appear as 'feature' node, so they must be reached via a for
+        //and check on each iteration if they are present
+        foreach ($xml_details->feature as $feature) {
+            if ($feature->taxon) {
+                $gene_details->Organism = $feature->taxon->attributes()->{'scientificName'};
+
+                //obtains the lineages
+                $feature_taxon = $feature->taxon;
+                if ($feature_taxon) {
+                    foreach ($feature_taxon->lineage->taxon as $lineage)
+                        $taxon_lineage[] = $lineage->attributes()->{'scientificName'};
+                    $gene_details->TaxonLineage = $taxon_lineage; //implode(', ', $taxon_lineage);
+                }
+            }
+            
+            if($feature->attributes()->{'name'} == 'CDS'){
+                $gene_details->CDS = $feature->attributes()->{'location'};
+            }
+        }
+
+        
         //and the sequence (without spaces)
         $gene_details->Sequence = preg_replace('/\s+/', '', $xml_details->sequence);
-        
-        
+
         //adds the taxon links (for organism and lineage)
         $gene_details->addTaxonLinks();
-        
+
         return $gene_details;
     }
-    
-    
+
     /**
      * Gives URL format to the properties $OrganismLink and $TaxonLineageLinks
      */
-    private function addTaxonLinks(){
-        $this->OrganismLink = 
-                '<a target="blank" href="' . 
-                EBIGeneDetails::$EBI_TAXON_URL . 
-                $this->Organism . '">' . 
+    private function addTaxonLinks() {
+        $this->OrganismLink =
+                '<a target="blank" href="' .
+                EBIGeneDetails::$EBI_TAXON_URL .
+                $this->Organism . '">' .
                 $this->Organism . '</a>';
-        
-        foreach ($this->TaxonLineage as $lineage){
+
+        foreach ($this->TaxonLineage as $lineage) {
             $lineage_links[] = '<a target="blank" href=' .
-                    EBIGeneDetails::$EBI_TAXON_URL . 
-                    $lineage . '">' . 
+                    EBIGeneDetails::$EBI_TAXON_URL .
+                    $lineage . '">' .
                     $lineage . '</a>';
         }
         $this->TaxonLineageLinks = implode(', ', $lineage_links);
     }
-    
+
     // </editor-fold>
     // </editor-fold>
-    
     // <editor-fold defaultstate="collapsed" desc="Private Attributes">
     private static $_Instance; //for singleton
     // </editor-fold>
-    
     // <editor-fold defaultstate="collapsed" desc="Constants (service URLs, parameter names...)">
     private static $EBI_GENE_DETAILS_URL = 'http://www.ebi.ac.uk/ena/data/view/';
     private static $EBI_TAXON_URL = 'http://www.ebi.ac.uk/ena/data/view/Taxon:';
+
     // </editor-fold>
 }
 
--- protected/modules/BLASTSearch/views/default/_genedetails.php
+++ protected/modules/BLASTSearch/views/default/_genedetails.php
@@ -4,47 +4,6 @@
  * @var $gene_details
  */
 
-//echo $gene_details;
-
-//foreach ($raw->children() as $child){
-  
-$child = $raw->children()->entry;
-
-    print_r($child->keyword);
-    echo '<br/><br/>';
-    print_r($child->reference);
-    echo '<br/><br/>';
-    print_r($child->feature);
-    echo '<br/><br/>';
-    print_r($child->sequence);
-    echo '<br/><br/>';
-//}
-
-echo '<br/><br/>';
-
-//print_r($raw);
-
-echo '<br/><br/>';
-
-echo 'Keyword:' . $gene_details->Keyword . '<br/>';
-//echo '$Reference:' . $gene_details->Reference . '<br/>';
-
-
-echo 'Title:' . $gene_details->Title . '<br/>';
-echo 'Author:' . $gene_details->Author . '<br/>';
-
-echo 'Organism: ' . $gene_details->OrganismLink . '<br/>';
-
-echo 'TaxonLineage: ' . $gene_details->TaxonLineageLinks . '<br/>';
-
-
-//echo '$Feature:' . $gene_details->Feature . '<br/>';
-//print_r($gene_details->Feature);
-echo '<br/><br/>';
-echo '$Sequence:' . $gene_details->Sequence . '<br/>';
-
-
-
 $this->widget('zii.widgets.CDetailView', array(
     'data'=>$gene_details,
     'attributes'=>array(
@@ -53,7 +12,7 @@ $this->widget('zii.widgets.CDetailView', array(
         'Author',
         'OrganismLink:html',
         'TaxonLineageLinks:html',
-        
+        'CDS',
         array(
             'label' => 'Sequence',
             'type' => 'raw',
