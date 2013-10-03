896dda11a7c69222ac1e6cd3f7dda5cc03d210d4
ofernandez <ofernandez@ubuntu>
2013-10-02 23:12:42 -0600

    ExPASy Connection- link to documentation

--- protected/modules/prosite/views/default/_view.php
+++ protected/modules/prosite/views/default/_view.php
@@ -14,11 +14,11 @@
     <br />
     
     <b><?php echo CHtml::encode('Signature_ac'); ?>:</b>
-        <?php echo CHtml::encode($data->signature_ac); ?>
+        <?php echo CHtml::link($data->signature_ac,'http://prosite.expasy.org/cgi-bin/prosite/nicedoc.pl?'.$data->signature_ac ,array('target'=>'_blank')) ?>
     <br />    
     
     <b><?php echo CHtml::encode('Score'); ?>:</b>
         <?php echo CHtml::encode($data->score); ?>
     <br />    
-        
+                  
 </div>
