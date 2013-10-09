a7d4bf0937f09e4bddd80683654a4176399e8d12
jpgarbanzo <jpgarbanzo@outlook.com>
2013-10-09 12:59:05 -0600

    Changed permissions on controllers in the main application. The permissions are now set to the users epa_master and research_master to delete, and all the actions can only be performed by authenticated users.

--- protected/controllers/PrimerController.php
+++ protected/controllers/PrimerController.php
@@ -27,17 +27,17 @@ class PrimerController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
+//			array('allow',  // allow all users to perform 'index' and 'view' actions
+//				'actions'=>array(),
+//				'users'=>array('*'),
+//			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update','filter'),
+				'actions'=>array('index','view','create','update', 'admin', 'filter'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+				'actions'=>array('delete'),
+				'users'=>array('epa_master','research_master'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
