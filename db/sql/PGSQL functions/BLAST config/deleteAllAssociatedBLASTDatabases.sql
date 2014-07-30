9549e025aef7e372c7d6e14f5727142e58d4c3b7
jpgarbanzo <jpgarbanzo@outlook.com>
2014-07-29 21:54:52 -0600

    Adding database related scripts, in 'db' folder.

---
+++ db/sql/PGSQL functions/BLAST config/deleteAllAssociatedBLASTDatabases.sql
@@ -0,0 +1,17 @@
+﻿ 
+CREATE OR REPLACE FUNCTION deleteAllAssociatedBLASTDatabases(
+    pConfigurationId integer)
+  RETURNS integer AS $$
+DECLARE
+    pConfigurationId ALIAS FOR $1;
+    
+BEGIN    
+
+    DELETE FROM tbl_ebidatabasesxblastuserconfiguration
+	WHERE idtbl_blastuserconfiguration = pConfigurationId;
+
+RETURN 1;
+
+END;
+
+$$ LANGUAGE plpgsql;
